37c78cad8a3f34173c1a6530c942f5ac7ded0e6c
RamziWafi-20 <ramziwafia@gmail.com>
2026-04-08 21:02:05 +0700

    Add files via upload

---
+++ backend/Data/materials.js
@@ -0,0 +1,7 @@
+const materials = [
+  { id: 1, title: "Budaya Indonesia Dasar", level: "beginner" },
+  { id: 2, title: "Tarian Tradisional", level: "intermediate" },
+  { id: 3, title: "Filosofi Budaya Nusantara", level: "advanced" },
+];
+
+module.exports = materials;
