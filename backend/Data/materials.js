d62f94cda558cc9f6ac032319b5978a06cf471fd
RamziWafi-20 <ramziwafia@gmail.com>
2026-04-09 20:39:22 +0700

    Delete backend/Data directory

--- backend/Data/materials.js
+++
@@ -1,7 +0,0 @@
-const materials = [
-  { id: 1, title: "Budaya Indonesia Dasar", level: "beginner" },
-  { id: 2, title: "Tarian Tradisional", level: "intermediate" },
-  { id: 3, title: "Filosofi Budaya Nusantara", level: "advanced" },
-];
-
-module.exports = materials;
